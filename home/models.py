6976212be2f687652f0d2c0518ab859b667531e4
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-06-23 17:15:36 +0530

    Creating models, register to admin and more

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=250)
+    date_added = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
